5be9cebe382ba6099ab9614fdadf8f9676ce5b83
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-04-22 12:04:15 +0200

    J&J på eventyr

--- src/App.js
+++ src/App.js
@@ -4,33 +4,139 @@ import About from './About';
 import Endpoint1 from './Endpoint1'
 import Endpoint2 from './Endpoint2'
 import Endpoint3 from './Endpoint3'
+import { Link } from 'react-router-dom'
 import Nav from './Nav'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react"
+import facade from "./apiFacade";
 
+function LogIn({ login }) {
+  const init = { username: "", password: "" };
+  const [loginCredentials, setLoginCredentials] = useState(init);
+
+  const performLogin = (evt) => {
+    console.log(loginCredentials)
+    evt.preventDefault();
+    login(loginCredentials.username, loginCredentials.password);
+  }
+  const onChange = (evt) => {
+    setLoginCredentials({ ...loginCredentials, [evt.target.id]: evt.target.value })
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onChange={onChange} >
+        <input placeholder="User Name" id="username" />
+        <input placeholder="Password" id="password" />
+        <button onClick={performLogin}>Login</button>
+      </form>
+    </div>
+  )
+
+}
+function LoggedIn() {
+  const [dataFromServer, setDataFromServer] = useState("Loading...")
+
+  useEffect(() => {
+    facade.fetchData().then(data => setDataFromServer(data.msg));
+  }, [])
+
+  return (
+    <div>
+      <h2>Data Received from server</h2>
+      <h3>{dataFromServer}</h3>
+    </div>
+  )
+
+}
 
 function App() {
-  return (
-    <Router>
-    <div className ="App">
-          <Nav />
-          <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/endpoint1" component={Endpoint1}/>
-          <Route path="/endpoint2" component={Endpoint2}/>
-          <Route path="/endpoint3" component={Endpoint3}/>
-          </Switch>
+  const [loggedIn, setLoggedIn] = useState(false)
 
-             </div>
-    </Router>
+  // function Logout () {
+  //   facade.logout()
+  //   setLoggedIn(false)
+  // }
+
+
+  const logout = () => {
+    setLoggedIn(false)
+    facade.logout();
+  }
+
+  const login = (user, pass) => {
+    LoginPage();
+    facade.login(user, pass)
+      .then(res => setLoggedIn(true));
+      
+  }
+ const logoutBtn = () => {
+    return (
+      <div> 
+        <p>Are you sure you want to log out?</p>
+        <Link to="/">
+     <button type="button" onClick={logout}>
+     Logout
+     </button>
+    </Link>
+        
+      </div>
+    )
+    
+  }
+
+
+ const ShowLandingPage = () => (
+  
+  <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/logout" component={logoutBtn}/>
+        <Route path="/login" component={LoginPage}/>
+        <Route path="/endpoint1" component={Endpoint1} />
+        <Route path="/endpoint2" component={Endpoint2} />
+        <Route path="/endpoint3" component={Endpoint3} />
+      </Switch>
+
+    </div>
+  </Router>
+  
+)
+function LoginPage(){
+  return(
+  !loggedIn ? (<LogIn login={login} />) :
+        (<div>
+          
+          <ShowLandingPage/>
+          
+        </div>)
+)}
+
+  return (
+
+
+    <div className="App">
+       {!loggedIn ? (<LogIn login={login} />) : 
+        (<div>
+          
+        <ShowLandingPage/>
+         
+          
+        </div>)}
+    </div>
   );
+ 
 }
 
 
 const Home = () => (
   <div>
-    <h1>home</h1>
+    <h1><LoggedIn/></h1>
   </div>
 )
 
-export default App;
+export default App; 
--- src/Nav.js
+++ src/Nav.js
@@ -1,45 +1,48 @@
 import './App.css';
 import React from 'react';
-import {Link} from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
+import facade from "./apiFacade";
 
 function Nav() {
 
     const navStyle = {
-        color:' white'
+        color: ' white'
     }
+   
 
-  return (
- <nav>
-     <Link style ={navStyle} to='/'>
-     <h3>Home</h3>
-         </Link>
+    return (
+        <nav>
+             
+            <Link style={navStyle} to='/'>
+                <h3>Home</h3>
+            </Link>
 
+           <ul className="links">
+                <Link style={navStyle} to="/logout">
+                    <li>logout</li>
+                </Link>
 
-            <h3>Login</h3>
-            <ul className="links">
-
-         <Link style={navStyle} to='/about'>
-            <li>about</li>
+                <Link style={navStyle} to='/about'>
+                    <li>about</li>
                 </Link>
 
 
-        <Link style={navStyle} to='/endpoint1'>
-             <li>Users</li>
+                <Link style={navStyle} to='/endpoint1'>
+                    <li>Users</li>
                 </Link>
 
-         <Link style={navStyle} to='/endpoint2'>
-             <li>Chuck norris jokes</li>
-                 </Link>
+                <Link style={navStyle} to='/endpoint2'>
+                    <li>Chuck norris jokes</li>
+                </Link>
 
-         <Link style={navStyle} to='/endpoint3'>
-             <li>Endpoint 3</li>
-                 </Link>
-         
-        
-     </ul>
- </nav>
-  );
+                <Link style={navStyle} to='/endpoint3'>
+                    <li>Endpoint 3</li>
+                </Link>
+
+
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
